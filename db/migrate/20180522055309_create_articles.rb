e8c970f297997b54aa5c39bc3655c62e33abde4c
Edmond Akpan <Edmond.Akpan@gmail.com>
2018-05-22 16:28:07 +0900

    completed wikinimous with no styling

---
+++ db/migrate/20180522055309_create_articles.rb
@@ -0,0 +1,10 @@
+rclass CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
